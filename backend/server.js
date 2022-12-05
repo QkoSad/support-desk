8fd0b86492f587612bc609962b5098e39ecd43cd
QkoSad <malakrapon@gmail.com>
2022-12-05 14:25:27 +0200

    created bacis structure and routes for creating user and logging in

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const colors = require("colors");
+const donenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleWare");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the support desk API" });
+});
+
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
